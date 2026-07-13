0174c33a5901a26e96a83f4f121786beba31c398
Abhas paul <abhaspaul825@gmail.com>
2026-07-14 01:02:34 +0530

    new proram

---
+++ src/Pattern_printing4.java
@@ -0,0 +1,13 @@
+public class Pattern_printing4 {
+    static void main() {
+
+        int n = 5;
+
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= n - row + 1 ; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern_printing5.java
@@ -0,0 +1,16 @@
+public class Pattern_printing5 {
+    static void main() {
+        int n = 5;
+
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= n - row; col++){
+                System.out.print("  ");
+            }
+            for(int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
