a2993b8a3af4312f08e3222f616fbf5bb6cebc97
Abhas paul <abhaspaul825@gmail.com>
2026-07-15 01:56:17 +0530

    New program

---
+++ src/Pattern_printing6.java
@@ -0,0 +1,16 @@
+public class Pattern_printing6 {
+    static void main() {
+
+        int n = 4;
+
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col < row; col++){
+                System.out.print("  ");
+            }
+            for(int col = 1; col <= 2 * (n - row) +1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
