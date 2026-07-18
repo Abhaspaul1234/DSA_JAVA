cfd82bd32212c5d8c38babaef292b82b82472e5e
Abhas paul <abhaspaul825@gmail.com>
2026-07-18 15:06:12 +0530

    new

--- src/Pattern_printing11.java
+++ src/Pattern_printing11.java
@@ -1,14 +1,31 @@
 public class Pattern_printing11 {
     static void main() {
 
-        int n = 5;
+        int n = 4;
 
         for(int row = 1; row <= n; row++){
             for(int col = 1; col <= n - row; col++){
-                    System.out.print(" ");
+                    System.out.print("  ");
             }
-            for(int col = 1; col <= n + row; col++){
-                System.out.print("* ");
+            for(int col = 1; col <= 2 * row - 1; col++){
+                if(col == 1 || col == 2 * row - 1) {
+                    System.out.print("* ");
+                }
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+        for(int row = n - 1; row >= 1; row--){
+            for(int col = 1; col <= n - row; col++){
+                System.out.print("  ");
+            }
+            for(int col = 1; col <= 2 * row - 1; col++){
+                if(col == 1 || col == 2 * row - 1) {
+                    System.out.print("* ");
+                }
+                else
+                    System.out.print("  ");
             }
             System.out.println();
         }
