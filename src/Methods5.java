03711ef6aaf06a80cb54b0c01366bc515fb9b252
Abhas paul <abhaspaul825@gmail.com>
2026-07-24 01:29:45 +0530

    new

---
+++ src/Methods5.java
@@ -0,0 +1,20 @@
+public class Methods5 {
+
+        static void solve(int num) {
+            System.out.println(num);
+
+            num = num * 10;
+
+            System.out.println(num);
+        }
+
+         static void main(String[] args) {
+            int num = 5;
+
+            System.out.println(num);
+
+            solve(num);
+
+            System.out.println(num);
+        }
+    }
