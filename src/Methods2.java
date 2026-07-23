e1542b46cf77e4aad5c2b62248c13ae0f94e4c2c
Abhas paul <abhaspaul825@gmail.com>
2026-07-23 16:25:10 +0530

    new

---
+++ src/Methods2.java
@@ -0,0 +1,10 @@
+public class Methods2 {
+
+    static void printSum(int x , int y) {
+        System.out.println("SUM: " + (x +y));
+    }
+    static void main() {
+        printSum(5, 10);
+
+    }
+}
---
+++ src/Methods3.java
@@ -0,0 +1,7 @@
+//public class Methods3 {
+//    static void main() {
+//
+//        static int add(int p, int q)
+//
+//    }
+//}
